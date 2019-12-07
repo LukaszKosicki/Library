e4472a422b1d1d788f753178537eaa34dbc53f8e
Lukasz <l.kijak@protonmail.com>
2019-12-07 16:31:42 +0100

    Add book, category and author model

---
+++ Models/Book/Model/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Book.Model
+{
+    public class Author
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
---
+++ Models/Book/Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Book.Model
+{
+    public class Category
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
