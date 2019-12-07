74b3b531b8a857c24f1250bf00e14286ed34b2fa
Lukasz <l.kijak@protonmail.com>
2019-12-07 14:13:02 +0100

    create admin account and default roles

--- Models/UserDbContext.cs
+++ Models/UserDbContext.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +15,42 @@ namespace Library.Models
         public UserDbContext(DbContextOptions<UserDbContext> options)
             : base(options) {}
 
-        
+        public async Task CreateAdminAndRoles(IServiceProvider provider,
+            IConfiguration configuration)
+        {
+            UserManager<AppUser> userManager = provider.GetRequiredService<UserManager<AppUser>>();
+            RoleManager<IdentityRole> roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string userRole = configuration["UserRole:User"];
+            string adminRole = configuration["UserRole:Admin"];
+
+            if (await roleManager.FindByNameAsync(userRole) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(userRole));
+            }
+            if (await roleManager.FindByNameAsync(adminRole) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(adminRole));
+            }
+
+            string login = configuration["Administrator:Login"];
+            string password = configuration["Administrator:Password"];
+
+            if (await userManager.FindByEmailAsync(login) == null)
+            {
+                AppUser user = new AppUser
+                {
+                    Email = login
+                };
+
+                IdentityResult result = await userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, userRole);
+                    await userManager.AddToRoleAsync(user, adminRole);
+                }
+            }
+        }
 
     }
 }
